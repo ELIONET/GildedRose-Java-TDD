6c96d00b8ed54d774be8184e2049399274c112e1
Emmanuel LIONET <emmanuel-lionet@hotmail.fr>
2015-11-03 08:26:57 +0100

    testInitialAreSellInAndQualityReduced -> Red GildedRose.updateItem() methods doesn't update items.

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,19 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class GildedRoseTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void testInitialAreSellInAndQualityReduced()
 	{
-		assertEquals(true,false);
+		Item item = new Item("Lame du Roi Déchu", 3200, 4);
+		Item copyItem = new Item("Lame du Roi Déchu", 3199, 3);
+		GildedRose.updateItem(item);
+		assertEquals(item, copyItem);
 	}
 
 }
