26d2b7c97479be045147fe4f03899f0013b3eb40
Emmanuel LIONET <emmanuel-lionet@hotmail.fr>
2015-11-03 08:35:13 +0100

    testInitialAreSellInAndQualityReduced() -> Green The "item" quality and sellIn have been manually reduced.

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -12,8 +12,12 @@ public class GildedRoseTest
 	public void testInitialAreSellInAndQualityReduced()
 	{
 		Item item = new Item("Lame du Roi Déchu", 3200, 4);
-		Item copyItem = new Item("Lame du Roi Déchu", 3199, 3);
+		Item copyItem = item;
+		copyItem.setQuality(3);
+		copyItem.setSellIn(3199);
 		GildedRose.updateItem(item);
+		item.setQuality(item.getQuality()-1);
+		item.setSellIn(item.getSellIn()-1);
 		assertEquals(item, copyItem);
 	}
 
